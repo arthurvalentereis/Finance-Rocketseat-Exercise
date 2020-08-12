30f170126ad52997219b08e7ff6d2aab8097ea12
arthur.valente.reis <arthur.valente.reis@gmail.com>
2020-08-12 00:48:50 -0300

    Import csv

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,6 +1,5 @@
 import { getCustomRepository } from 'typeorm';
 import AppError from '../errors/AppError';
-import Transaction from '../models/Transaction';
 import Transactionrepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
